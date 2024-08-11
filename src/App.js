5121964ecd19c12ab5d0ef9469a2d622a66daafa
Mykola Dzoban <nick.dzoban@gmail.com>
2024-08-11 16:45:17 +0300

    added subscription to messages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import ChatContent from './components/ChatContent';
 import ChatFooter from './components/ChatFooter';
 import ChatHeader from './components/ChatHeader';
 import ChatRules from './components/ChatRules';
 import OnlineOffline from './components/OnlineOffline';
-import Timer from './components/Timer';
+import { firebaseCollections, firebaseFirestore } from './config/firebaseConfig';
 
 function App() {
+	const [allMessages, setAllMessages] = useState([]);
+
+	// subscribe to messages changes
+	useEffect(() => {
+		const q = query(
+			collection(firebaseFirestore, firebaseCollections.messages)
+			// where(ChatTypeFieldsEnum.INSTANCE_ID, '==', idToFetch),
+		);
+		const unsub = onSnapshot(q, (snapshot) => {
+			const messages = snapshot.docs.map((doc) => ({
+				...doc.data(),
+				id: doc.id,
+			}));
+
+			setAllMessages(messages);
+		});
+		return () => {
+			unsub();
+		};
+	}, [setAllMessages]);
+
 	return (
 		<div className="container mx-auto">
 			<div className="flex flex-col items-center gap-4 justify-center">
@@ -15,12 +38,12 @@ function App() {
 				<OnlineOffline />
 				{/* <Timer /> */}
 			</div>
-			<div className='flex items-center justify-center w-full'>
+			<div className="flex items-center justify-center w-full">
 				<ChatRules />
 			</div>
-			<div className='flex flex-col items-center justify-center w-full'>
+			<div className="flex flex-col items-center justify-center w-full">
 				<ChatHeader />
-				<ChatContent />
+				<ChatContent messages={allMessages} />
 				<ChatFooter />
 			</div>
 		</div>
--- src/components/ChatContent.js
+++ src/components/ChatContent.js
@@ -1,6 +1,5 @@
-const ChatContent = () => {
-  return (
-    <div className="w-full p-2 border-x-2 border-slate-300 h-[350px]">ChatContent</div>
-  )
-}
-export default ChatContent
+const ChatContent = ({ messages, ...props }) => {
+	console.log(messages);
+	return <div className="w-full p-2 border-x-2 border-slate-300 h-[350px]">ChatContent</div>;
+};
+export default ChatContent;
