548a1b4407b4b0457ce7356c65fd8c956d54e53a
Mykola Dzoban <nick.dzoban@gmail.com>
2024-08-12 11:41:10 +0300

    changed structure in components folder

---
+++ src/components/chat/ChatFooter.js
@@ -0,0 +1,67 @@
+import { messages } from '../../config/firebaseConfig';
+import { toast } from '../../modules/toast/Toast';
+
+const ChatFooter = () => {
+	const onSubmit = async (e) => {
+		e.preventDefault();
+
+		const message = e.target.elements.message.value;
+		const loverMSG = message.toLowerCase();
+		if (
+			message &&
+			(loverMSG.includes('fuck') ||
+				loverMSG.includes('shit') ||
+				loverMSG.includes('ass') ||
+				loverMSG.includes('asshole') ||
+				loverMSG.includes('bitch') ||
+				loverMSG.includes('dick') ||
+				loverMSG.includes('cunt') ||
+				loverMSG.includes('pussy') ||
+				loverMSG.includes('penis') ||
+				loverMSG.includes('vagina') ||
+				loverMSG.includes('блять') ||
+				loverMSG.includes('пизда') ||
+				loverMSG.includes('сука') ||
+				loverMSG.includes('хуй') ||
+				loverMSG.includes('дурак') ||
+				loverMSG.includes('пидор') ||
+				loverMSG.includes('пидр') ||
+				loverMSG.includes('пидрас') ||
+				loverMSG.includes('пидарас') ||
+				loverMSG.includes('підор') ||
+				loverMSG.includes('підр') ||
+				loverMSG.includes('підрас') ||
+				loverMSG.includes('пісюн') ||
+				loverMSG.includes('піська') ||
+				loverMSG.includes('підарас'))
+		) {
+			console.log('you have been blocked');
+		}
+
+		// send message
+		if (message && navigator.onLine) {
+			await messages
+				.create({
+					message,
+					date: new Date().toISOString(),
+				})
+				.then(() => console.log('sent'));
+
+			toast.success({ message: 'Message sent' });
+		}
+
+		e.target.reset();
+	};
+
+	return (
+		<div className="w-full border-2 rounded-b-md border-slate-300 py-3">
+			<form action="post" onSubmit={onSubmit} className="w-full flex items-center">
+				<input type="text" name="message" className="w-full rounded-lg border-2 border-dashed border-emerald-900" />
+				<button className="w-fit text-center rounded-lg" disabled={!navigator.onLine}>
+					<img src="send.svg" alt="Send" className="size-10" />
+				</button>
+			</form>
+		</div>
+	);
+};
+export default ChatFooter;
---
+++ src/components/chat/ChatRules.js
@@ -0,0 +1,12 @@
+const ChatRules = () => {
+  return (
+    <div>
+      <ul className="flex flex-col">
+        <li>first</li>
+        <li>second</li>
+        <li>third</li>
+      </ul>
+    </div>
+  )
+}
+export default ChatRules
