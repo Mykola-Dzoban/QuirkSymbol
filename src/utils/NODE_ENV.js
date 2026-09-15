cc46ff2571b14dcc7031cb81f7ddd6af1e270b0a
MD <dzeban03@gmail.com>
2026-09-15 17:33:45 +0300

    feat: change project purpose - excalidraw analog

--- src/utils/NODE_ENV.js
+++
@@ -1,3 +0,0 @@
-export const NODE_ENV = process.env.NODE_ENV === 'development' ? 'development' : 'production'
-export const NODE_ENV_DEV = NODE_ENV === 'development'
-export const NODE_ENV_PROD = NODE_ENV === 'production'
